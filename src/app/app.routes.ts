391bd1f6d66ec3e12e3d0083c3d56d3ee541c199
harshita ramani <harshitaramani18@gmail.com>
2024-03-20 12:26:15 +0530

    random

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  {path:'**', component: NotfoundComponent}
+];
